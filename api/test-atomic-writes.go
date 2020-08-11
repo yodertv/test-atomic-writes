8fad5c2763f4e2c9f663be32eac63e6eb29da520
Michael Yoder <yodercode@gmail.com>
2020-08-10 23:21:56 -0400

    Added a test for the API to force it to compile with out having to deploy.

--- api/test-atomic-writes.go
+++ api/test-atomic-writes.go
@@ -2,10 +2,12 @@ package handler
 
 import (
 	"os"
+	"io"
     "fmt"
     "time"
     "syscall"
     "runtime"
+    "os/exec"
     "net/http"
 )
 
@@ -15,4 +17,28 @@ func Handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "%#v\n", runtime.GOOS)
     fmt.Fprintf(w, "%#v\n", os.Environ())
     fmt.Fprintf(w, "pagesize=%d\n", syscall.Getpagesize())
+	var cmdString []string
+    cmd := cmdMake(w, "../test-atomic-writes", cmdString)
+	err := cmd.Start()
+	if err != nil { fmt.Fprintf(w, " Start failed: %#v\n", err) }
+	err = cmd.Wait()
+	if err != nil { fmt.Fprintf(w, "Wait failed: %#v\n", err) }
+}
+
+// subPscat returns an exec.Cmd with the pscat subtest set up to be started or run with the subcommand arguments.
+// It copies the io of the parent. It marks the environment with a "TESTING" flag.
+// Should the pscat command care to check for the flag, it could do so like:
+//    if os.Getenv("PSCAT_TESTING_IN_PROGRESS") == "1" {
+//       fmt.Println("Testing in progress!")
+//    }
+func cmdMake(w io.Writer, s string, args []string) *exec.Cmd {
+    env := []string{
+		"CMD_IN_PROGRESS=1",
+    }
+    cmd := exec.Command(s, args...)
+    cmd.Env = append(os.Environ(), env...)
+    cmd.Stdin = os.Stdin
+    cmd.Stdout = w
+    cmd.Stderr = os.Stderr
+    return cmd
 }
---
+++ api/test-atomic-writes_test.go
@@ -0,0 +1,35 @@
+// test-atomic-writes_test.go
+
+package handler
+
+import (
+	"testing"
+	"net/http"
+    "net/http/httptest"
+)
+
+func TestAtomicWriteHandler(t *testing.T) {
+    // Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+    // pass 'nil' as the third parameter.
+    req, err := http.NewRequest("GET", "/api/test-atomic-writes", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+    // We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(Handler)
+    // Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+    // directly and pass in our Request and ResponseRecorder.
+    handler.ServeHTTP(rr, req)
+    // Check the status code is what we expect.
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf("handler returned wrong status code: got %v want %v",
+            status, http.StatusOK)
+    }
+    // Check the response body is what we expect.
+    expected := 100
+    if len(rr.Body.String()) < expected {
+        t.Errorf("handler returned unexpected body size: got %v want %v",
+            len(rr.Body.String()), expected)
+    }
+}
