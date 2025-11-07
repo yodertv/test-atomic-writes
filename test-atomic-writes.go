7089e5797f445568a79236ff42f87d763d404b61
Michael Yoder <yodercode@gmail.com>
2025-11-06 22:36:17 -0500

    Added back in comandline argument error handling.

--- test-atomic-writes.go
+++ test-atomic-writes.go
@@ -67,7 +67,37 @@ func close_log (fd int, file string){
 }
 
 func main() {
-    test_atomic_writes.Write_bytes(count, size, workers, worker, false, filename)
-    res := test_atomic_writes.Validate_bytes(filename, count, size, workers)
+	var (
+	    res int = 0
+	    count int
+	    size int
+	    workers int
+	    worker int
+	    readonly bool
+	    filename string
+	)
+    flag.StringVar(&filename, "f", "out.tmp", "f(ilename) to use for test")
+    flag.IntVar(&count, "c", 10, "c(ount): of writes per worker, use at least 1")
+    flag.IntVar(&size, "s", 30, "s(size): in bytes to write, use at least 2")
+    flag.IntVar(&workers, "w", 4, "w(orkers): number of concurent writers, use at least 1 and no more than 222")
+    flag.BoolVar(&readonly, "readonly", false, "readonly: just run the validate function, use identical flags")
+    flag.IntVar(&worker, "worker", -1, "worker: perform the writes, internal")
+    flag.Parse()
+	if len(flag.Args()) != 0 { // Extraneous arguments
+		flag.Usage();
+		os.Exit(1)
+	}
+	if workers > 222 || workers < 1 || count < 1 || size < 2 {
+		fmt.Println("Between 1 and 222 workers, writing at least 1 count each, of at least size 2 required to test.")
+		flag.Usage();
+		os.Exit(1)
+	}
+	if (!readonly){
+		// Note that this function fork a process for each worker.
+		test_atomic_writes.Write_bytes(count, size, workers, worker, false, filename)
+    }
+    if worker == -1 { // The orchestrating process has worker index = -1. Only need to validate the file once after all the workers finish.
+	    res = test_atomic_writes.Validate_bytes(filename, count, size, workers)
+    }
     os.Exit(res)
 }
